117309e5c1cb99672219f4ec0e99a970a2da3a09
PolinaScrbbs <147543684+PolinaScrbbs@users.noreply.github.com>
2024-10-23 11:28:19 +0300

    Debug False

--- yolo_detection/settings.py
+++ yolo_detection/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-26efj!6*^0i(oe$y&vxwvga3tz+iyu#zbl+dz_+*wo+$ymx-)3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
 ALLOWED_HOSTS = []
 
